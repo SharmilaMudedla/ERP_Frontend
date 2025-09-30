6b210bc166cd934195b619dad26d831235a78df5
SharmilaMudedla <sharmila@spondias.com>
2025-09-30 12:38:52 +0530

    Update httpClient.js

--- src/Utils/httpClient.js
+++ src/Utils/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: https://erp-backendapi-9upv.onrender.com,
+  baseURL: "https://erp-backendapi-9upv.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
